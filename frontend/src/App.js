c70cd02cf69f18caa369fe5a1f2244f968f291aa
sujitI2interfaces <sujit@i2interfaces.com>
2025-01-02 13:47:33 +0530

    commit changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,22 +4,18 @@ import Navbar from "./Components/Navbar";
 import Login from "./Components/Login";
 import Logout from "./Components/Logout";
 import Home from "./Components/Home";
-import Signup from "./Components/Signup";
-
-  
 
 function App() {
-    return (
-        <Router>
-            <Navbar />
-            <Routes>
-                <Route path="/login" element={<Login />} />
-                <Route path="/signup" element={<Signup />} />
-                <Route path="/logout" element={<Logout />} />
-                <Route path="/" element={<Home />} />
-            </Routes>
-        </Router>
-    );
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/logout" element={<Logout />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;

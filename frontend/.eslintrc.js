731ed7468657e164c86c81f0dcf546d2fbc044d8
sujitI2interfaces <sujit@i2interfaces.com>
2025-01-02 13:44:44 +0530

    lint

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  extends: [
+    "eslint:recommended", // or any other base configuration you are using (e.g., 'react-app')
+  ],
+  rules: {
+    "no-labels": "off",
+    "default-case": "warn",
+    "no-unused-expressions": "warn",
+    "no-sequences": "warn",
+    "no-mixed-operators": "warn",
+  },
+};
